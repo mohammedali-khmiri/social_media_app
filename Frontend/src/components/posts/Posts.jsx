61167d70401923d5986837ef671d1e148219823a
medAli <khemirimohamedali7@gmail.com>
2023-02-07 14:55:24 +0100

    post commit

--- Frontend/src/components/posts/Posts.jsx
+++ Frontend/src/components/posts/Posts.jsx
@@ -27,6 +27,7 @@ const Posts = () => {
 			userId: 3,
 			profilePic:
 				"https://images.pexels.com/photos/12242014/pexels-photo-12242014.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+			desc: "💙",
 			img: "https://media.istockphoto.com/photos/beautiful-young-couple-relaxing-after-hiking-and-taking-a-break-picture-id1096035138?b=1&k=20&m=1096035138&s=612x612&w=0&h=ogsX2MjUZp0mtHhw7YubUpbPVtsSGHvG8v-VIvAx4_4=",
 		},
 	];
